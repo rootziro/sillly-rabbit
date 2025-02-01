1a561b1f8492d9576fcf1cb63dc7f9601db2c62a
rootziro <sploit55@gmail.com>
2025-02-01 23:39:21 +0000

    commited

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import string
 # Generate a secure random password length of 20
 
 password_length = 20
-characters = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
+characters = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
+
+# Secure random password
 password = ''.join(secrets.choice(characters) for i in range(password_length))
 
 print("Password Generated", password)
