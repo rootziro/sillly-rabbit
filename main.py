ddc64d5d2b7bb7199e4750f2d570a53962abfefc
rootziro <sploit55@gmail.com>
2025-02-02 22:49:53 +0000

    comments removed

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ characters = string.ascii_letters + string.ascii_lowercase + string.ascii_upperc
 # Secure random password
 password = ''.join(secrets.choice(characters) for i in range(password_length))
 
-#Generate random integer
 secure_random_int = secrets.randbelow(100)
 
 #Generate secure URL-safe token
@@ -19,5 +18,4 @@ print("Password Generated", password)
 print("Secure Random Integer", secure_random_int)
 print("Secure Token", secure_token)
 
-#Store passwd
-store_password(password)
+store_password(password)    
