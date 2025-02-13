5d9cae63785c7a82d2a0f4d7e3264d36e27bb2d3
rootziro <sploit55@gmail.com>
2025-02-13 20:27:37 +0000

    Flask function created for generation of passwords. main will manage scheduling

--- Backend/app.py
+++ Backend/app.py
@@ -5,3 +5,18 @@ from storage import store_password
 
 app = Flask(__name__) # Call function whenever a request is made to the root url.
 app.route('/generate-password', methods=['POST'])
+
+def generate_password():
+    password_length = 20
+    characters = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
+    password = ''.join(secrets.choice(characters) for i in range(password_length))
+    secure_random_int = secrets.randbelow(100)
+    secure_token = secrets.token_urlsafe(20)
+
+    store_password(password)
+
+    return jsonify({
+        'password': password,
+        'secure_random_int': secure_random_int,
+        'secure_token': secure_token
+    })
